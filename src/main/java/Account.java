43f1b19078cec03f2c50c1177d9eb7b6dc1e8cbe
nilsoriano <nilsorianovila@gmail.com>
2022-08-13 23:39:13 +0200

    Included today's work Also fixed some directory issues to prevent conflicts.

---
+++ src/main/java/Account.java
@@ -0,0 +1,77 @@
+import java.util.List;
+
+public class Account {
+    private Integer id; // we may handle ids externally, this could be removed
+    private Enum industry;
+    private int employeeCount;
+    private String city;
+    private String country;
+    private List<Contact> contactList;
+    private List<Opportunity> opportunityList;
+
+    public Account(Integer id, Enum industry, int employeeCount, String city, String country, List<Contact> contactList, List<Opportunity> opportunityList) {
+        setId(id);
+        setIndustry(industry);
+        setEmployeeCount(employeeCount);
+        setCity(city);
+        setCountry(country);
+        setContactList(contactList);
+        setOpportunityList(opportunityList);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Enum getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(Enum industry) {
+        this.industry = industry;
+    }
+
+    public int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(int employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Contact> getContactList() {
+        return contactList;
+    }
+
+    public void setContactList(List<Contact> contactList) {
+        this.contactList = contactList;
+    }
+
+    public List<Opportunity> getOpportunityList() {
+        return opportunityList;
+    }
+
+    public void setOpportunityList(List<Opportunity> opportunityList) {
+        this.opportunityList = opportunityList;
+    }
+}
---
+++ src/main/java/Contact.java
@@ -0,0 +1,9 @@
+public class Contact extends Lead {
+
+
+    public Contact(String name, int phoneNumber, String email, String companyName) {
+        super(name, phoneNumber, email, companyName);
+    }
+
+
+}
