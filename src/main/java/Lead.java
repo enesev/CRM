47972671535816502e71f634aea6df4c53354e71
sergiolara92 <sergioldm92@gmail.com>
2022-08-13 19:57:57 +0200

    clases

---
+++ src/main/java/Lead.java
@@ -0,0 +1,56 @@
+public class Lead {
+
+
+    private String name;
+    private int phoneNumber;
+    private String email;
+    private String companyName;
+
+    public Lead(String name, int phoneNumber, String email, String companyName) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.companyName = companyName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+
+
+    public static Integer createID(Integer idCounter){
+        Integer autosum = idCounter++;
+        return autosum;
+    }
+}
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //TODO ESTO SON PRUEBAS//
+
+
+    Integer idCounter = Lead.createID(0);
+        System.out.println(idCounter);
+
+    Map<Integer, Lead> leadLists = new HashMap<>();
+
+        Lead lead1 = new Lead("juan", 555, "eee", "ddd");
+        Lead lead2 = new Lead("juan88", 555, "eee", "ddd");
+
+        leadLists.put(Lead.createID(idCounter), lead1);
+
+        leadLists.put(Lead.createID(idCounter), lead2);
+        System.out.println(leadLists.keySet());
+
+
+        System.out.println(leadLists.get(0).getName());
+        System.out.println(leadLists.get(1).getName());
+
+        Map<Integer, Lead> contacts = new HashMap<>();
+
+
+        leadLists.remove(0);
+
+        //HASTA AQUI SON PRUEBAS //
+
     }
 }
